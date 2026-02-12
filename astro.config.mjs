72259bc7dff8188fb24672a32f810520edfc25af
Knrc <contact@knrc.art>
2026-02-13 00:30:56 +0100

    fix source folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.knrc.art",
-  base: '/knrc.art',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
